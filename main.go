5fe47c52bcf7bb820f6194197024e7cc6d0b3c3a
Hui Kang <kangh@us.ibm.com>
2017-03-24 16:58:18 +0000

    update

--- main.go
+++ main.go
@@ -9,72 +9,14 @@ import (
 	"github.ibm.com/kangh/libnetwork-ovn-plugin/ovn"
 )
 
-type MyNetworkDriver struct {
-	val int
-}
-
 func main() {
 	fmt.Println("Libnetwork ovn plugin")
-	fmt.Println(ovn.DefaultRoute)
 
-	d := MyNetworkDriver{}
+	d, err := ovn.NewDriver()
+	if err != nil {
+		panic(err)
+	}
 
 	h := network.NewHandler(d)
-	h.ServeTCP("ovn", ":8087", nil)
-}
-
-func (d MyNetworkDriver) AllocateNetwork(req *network.AllocateNetworkRequest) (*network.AllocateNetworkResponse, error) {
-	return nil, nil
-}
-
-func (d MyNetworkDriver) CreateEndpoint(req *network.CreateEndpointRequest) (*network.CreateEndpointResponse, error) {
-	return nil, nil
-}
-
-func (d MyNetworkDriver) DeleteEndpoint(req *network.DeleteEndpointRequest) error {
-	return nil
-}
-
-func (d MyNetworkDriver) CreateNetwork(req *network.CreateNetworkRequest) error {
-	return nil
-}
-
-func (d MyNetworkDriver) DeleteNetwork(req *network.DeleteNetworkRequest) error {
-	return nil
-}
-
-func (d MyNetworkDriver) DiscoverDelete(req *network.DiscoveryNotification) error {
-	return nil
-}
-
-func (d MyNetworkDriver) DiscoverNew(notif *network.DiscoveryNotification) error {
-	return nil
-}
-
-func (d MyNetworkDriver) EndpointInfo(req *network.InfoRequest) (*network.InfoResponse, error) {
-	return nil, nil
-}
-
-func (d MyNetworkDriver) FreeNetwork(req *network.FreeNetworkRequest) error {
-	return nil
-}
-
-func (d MyNetworkDriver) GetCapabilities() (*network.CapabilitiesResponse, error) {
-	return nil, nil
-}
-
-func (d MyNetworkDriver) Join(req *network.JoinRequest) (*network.JoinResponse, error) {
-	return nil, nil
-}
-
-func (d MyNetworkDriver) Leave(req *network.LeaveRequest) error {
-	return nil
-}
-
-func (d MyNetworkDriver) ProgramExternalConnectivity(req *network.ProgramExternalConnectivityRequest) error {
-	return nil
-}
-
-func (d MyNetworkDriver) RevokeExternalConnectivity(req *network.RevokeExternalConnectivityRequest) error {
-	return nil
+	h.ServeUnix(ovn.DriverName, 0)
 }
--- ovn/driver.go
+++ ovn/driver.go
@@ -1,5 +1,98 @@
 package ovn
 
-const (
-	DefaultRoute = "0.0.0.0/0"
+import (
+	"fmt"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/docker/go-plugins-helpers/network"
+	"github.com/samalba/dockerclient"
 )
+
+const (
+	DriverName = "ovn"
+)
+
+type dockerer struct {
+	client *dockerclient.DockerClient
+}
+
+type Driver struct {
+	val int
+	dockerer
+}
+
+func NewDriver() (*Driver, error) {
+	docker, err := dockerclient.NewDockerClient("unix:///var/run/docker.sock", nil)
+        if err != nil {
+		return nil, fmt.Errorf("could not connect to docker: %s", err)
+	}
+	
+	d := &Driver{
+		dockerer: dockerer{
+			client: docker,
+		},
+	}
+	return d, nil
+}
+
+func (d Driver) AllocateNetwork(req *network.AllocateNetworkRequest) (*network.AllocateNetworkResponse, error) {
+	log.Debugf("Allocate network request: %+v", req)
+	res := &network.AllocateNetworkResponse{
+		Options: make(map[string]string),
+	}
+	return res,nil
+}
+
+func (d Driver) CreateEndpoint(req *network.CreateEndpointRequest) (*network.CreateEndpointResponse, error) {
+	log.Debugf("Create network request: %+v", req)
+	fmt.Println("Create network request", req)
+	return nil, nil
+}
+
+func (d Driver) DeleteEndpoint(req *network.DeleteEndpointRequest) error {
+	return nil
+}
+
+func (d Driver) CreateNetwork(req *network.CreateNetworkRequest) error {
+	return nil
+}
+
+func (d Driver) DeleteNetwork(req *network.DeleteNetworkRequest) error {
+	return nil
+}
+
+func (d Driver) DiscoverDelete(req *network.DiscoveryNotification) error {
+	return nil
+}
+
+func (d Driver) DiscoverNew(notif *network.DiscoveryNotification) error {
+	return nil
+}
+
+func (d Driver) EndpointInfo(req *network.InfoRequest) (*network.InfoResponse, error) {
+	return nil, nil
+}
+
+func (d Driver) FreeNetwork(req *network.FreeNetworkRequest) error {
+	return nil
+}
+
+func (d Driver) GetCapabilities() (*network.CapabilitiesResponse, error) {
+	return nil, nil
+}
+
+func (d Driver) Join(req *network.JoinRequest) (*network.JoinResponse, error) {
+	return nil, nil
+}
+
+func (d Driver) Leave(req *network.LeaveRequest) error {
+	return nil
+}
+
+func (d Driver) ProgramExternalConnectivity(req *network.ProgramExternalConnectivityRequest) error {
+	return nil
+}
+
+func (d Driver) RevokeExternalConnectivity(req *network.RevokeExternalConnectivityRequest) error {
+	return nil
+}
